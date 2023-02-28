1632cf3f279cd359b90aa7334ed4d970422fc1db
sachin <sachintrivedi221.com>
2023-02-28 10:23:01 +0530

    ïnit

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-import dotenv from "dotenv"
-import  express from "express";
-import mongoose from "mongoose";
-import { connectDB } from "./connection.js";
-const app=express()
-connectDB()
-
-dotenv.config({path:"./config.env"})
-const PORT =process.env.PORT
-
-app.use(express.json())
-// all router files
-import auth from "./routes/auth.js"
-app.use(auth)
+import app from "./app.js";
+import { connectDB } from "./config/database.js";
 
 
+connectDB();
 
 
-
-
-
-
-
-
-
-app.listen(PORT,(req,res)=>{
-    console.log(`server is running port number ${PORT}`)
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is working on port :${process.env.PORT}`)
 })
-
-
